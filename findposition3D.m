b1df10add3bc0813e7924b61e79af1d832d25e3f
Ingie Hong <ingiehong@gmail.com>
2016-02-24 13:59:17 -0500

    Resolution fix and text fix in findposition3D().

--- findposition3D.m
+++ findposition3D.m
@@ -3,8 +3,8 @@
 % 
 
 % We should implement image header information import for RESOLUTION info
-fixed_image = scimat_im2scimat(reshape(fixed_image, size(fixed_image, 1), size(fixed_image, 2), size(fixed_image, 3), 1, size(fixed_image,4)), [0.9 0.9 2] );
-moving_image = scimat_im2scimat(reshape(moving_image, size(moving_image, 1), size(moving_image, 2), size(moving_image, 3), 1, size(moving_image,4)), [0.9 0.9 2]);
+fixed_image = scimat_im2scimat(reshape(fixed_image, size(fixed_image, 1), size(fixed_image, 2), size(fixed_image, 3), 1, size(fixed_image,4)), [389/512 389/512 1] );
+moving_image = scimat_im2scimat(reshape(moving_image, size(moving_image, 1), size(moving_image, 2), size(moving_image, 3), 1, size(moving_image,4)), [389/512 389/512 1]);
 % Execute registration through Elastix wrapper from Oxford
 disp('3D Registration initiated...')
 [transformation, fit, registered_image] = reg3D(fixed_image, moving_image);
@@ -23,7 +23,7 @@ title('Front view')
 
 % Present transformation parameters
 %min_fit_i
-disp([num2str(round(10*transformation(6))/10) 'um shift between moving and fixed reference image corresponds to best registration.' ]);
+disp([num2str(round(10*transformation(6))/10) 'um Z-shift between moving and fixed reference image corresponds to best registration.' ]);
 % %transformation(min_fit_i, :)
 disp(['Transformation X-Y-Z (um): ' num2str(round(10*transformation(4:6))/10)]);
 disp(['Euler Angles (deg, X-Y-Z axis): ' num2str(round(-10*transformation(1:3)*360/pi/2)/10)])
