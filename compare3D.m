acec3c9b91ba161a03d83eaf816b14f980932834
Ingie Hong <ingiehong@gmail.com>
2016-03-23 10:47:51 -0400

    Updated compare3D for side-by-side 'falsecolor' and 'diff' display.

--- compare3D.m
+++ compare3D.m
@@ -1,7 +1,18 @@
 
 % Use imshowpair to visualize registration results
-figure
+
+scrsz = get(groot,'ScreenSize');
+figure('Position',[50 50 scrsz(3)-100 scrsz(4)-200])
+set(gcf,'Units','normal')
+    
 for i=1:size(registered_image,3)
-imshowpair(fixed_image.data(:,:,i),registered_image(:,:,i), 'montage')
-pause;
+    subplot(1,2,1)
+    set(gca,'Position',[0 0 0.5 1])
+    imshowpair(fixed_image.data(:,:,i),registered_image(:,:,i)); %, 'montage')
+    subplot(1,2,2)
+    set(gca,'Position',[0.5 0 0.5 1])
+    imshowpair(fixed_image.data(:,:,i),registered_image(:,:,i), 'diff'); %, 'montage')
+    pause;
 end
+
+close(gcf)
