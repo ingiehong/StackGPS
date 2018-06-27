9e193aa70a35de3abb78379ecb75ae1384a9ebf4
Ingie Hong <ingiehong@jhmi.edu>
2018-06-27 17:21:59 -0400

    Upgraded highpassfilt3 to allow multi-color images.

--- highpassfilt3.m
+++ highpassfilt3.m
@@ -1,4 +1,4 @@
-function filtered_image = highpassfilt3(raw_image,sigma) 
+function filtered_images = highpassfilt3(raw_image,sigma) 
 % Highpass-filters 3d images by subtracting a gaussian filtered image
 % 
 % raw_image: uint16 3d image
@@ -6,6 +6,8 @@ function filtered_image = highpassfilt3(raw_image,sigma)
 %
 % Ingie Hong, Johns Hopkins Medical Institute, 2016
 
-smoothed_image = imgaussfilt3(raw_image,sigma);
-filtered_image = int16(raw_image)- int16(smoothed_image);
-filtered_image = filtered_image - min(filtered_image(:));
+for i=1:size(raw_image,5)
+    smoothed_image = imgaussfilt3(raw_image(:,:,:,:,i),sigma);
+    filtered_image = int16(raw_image(:,:,:,:,i))- int16(smoothed_image);
+    filtered_images(:,:,:,:,i) = filtered_image - min(filtered_image(:));
+end
