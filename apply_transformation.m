46c402b18fe3f25a7042c384f7ae04ff47b77763
Ingie Hong <ingiehong@gmail.com>
2018-11-21 00:27:15 -0500

    Expanded apply_transformation() to accept momory-loaded image arrays.

--- apply_transformation.m
+++ apply_transformation.m
@@ -1,12 +1,21 @@
-function [transformed_image, target_image] = apply_transformation( target_FileName, target_channel, target_res, t) 
+function [transformed_image, target_image] = apply_transformation( target_FileName, target_channel, target_res, t, target_image) 
 % Function to transform other images with the transformation discovered with
 % registration
+%
+% target_FileName is the source image filename, for use of loading and also
+% saving with '_transformed.tif' postfix
 %
 % ex) [transformed_image, target_image] = apply_transformation(fullfile(sg.movingPathName,sg.movingFileName), 2, sg.moving_res, sg.t);
 %
 % Ingie Hong, Johns Hopkins Medical Institute, 2018
 
-target_image = tiffclassreader( target_FileName, target_channel);
+
+if nargin < 5 || isempty(target_image) 
+    target_image = tiffclassreader( target_FileName, target_channel);
+else
+    disp('Using image provided...') 
+end
+  
 t.ResultImageFormat = 'mha';
 transformed_image = transformix( t, scimat_im2scimat(target_image, target_res));
 disp([ 'Original image dynamic range = ' num2str( [ min(target_image(:)) max(target_image(:))]) ]);
