98793f30da2bc3852bbf7f343f79cae7bd1e979a
Ingie Hong <ingiehong@gmail.com>
2019-03-04 16:30:14 -0500

    Revamped tiffclassreader to use OME *.xml support for Prarie View/Bruker images. Added automatic resolution detection for OME loading.

--- tiffclassreader.m
+++ tiffclassreader.m
@@ -1,93 +1,63 @@
-function imageStack = tiffclassreader(fname, channels)
-% Simple TIF reader for Scanimage or Prarie 2P tif/tiff images/stacks
-% For other microscopy image formats, use Open Microscopy BIO-FORMATS toolbox
+function [imageStack, pixel_size] = tiffclassreader(fname, channels)
+% Fast and simple TIF reader for Scanimage or Bruker(Prarie View) 2P tif/tiff images/stacks
+% For other microscopy image formats, the reader uses Open Microscopy
+% BIO-FORMATS toolbox to import data.
 %
 % fname : full filename of image. ex) '2p_image_file.tif';
 % channels: if set as 0 then all channels are returned, otherwise channels
 %           defined are returned ex) [1 2]
 %
-% Ingie Hong, Johns Hopkins Medical Institute, 2016
+% Ingie Hong, Johns Hopkins Medical Institute, 2016-2019
 
 [filepath,name,ext]  = fileparts(fname);
 ext = lower(strrep(ext, '.', ''));
 
+if nargin<2 || isempty(channels) 
+    disp('Multicolor (or timelapse) image detected. Using first color only.')
+    channels = 1;
+end
+
 switch ext
     case 'tif'
 
-        if isempty( regexp(fname,'ZSeries') ) || isempty( regexp(fname,'_Ch') )    % Check if Prarie-style zstack file series...
+        % for generic multi-stack tiffs
+        info = imfinfo(fname);
+        numberOfImages = length(info);
+        %[info(1).Width info(1).Height numberOfImages]
 
-            % for generic multi-stack tiffs
-            info = imfinfo(fname);
-            numberOfImages = length(info);
-            %[info(1).Width info(1).Height numberOfImages]
+        if isfield(info(1), 'ImageDescription') && ~isempty(strfind(info(1).ImageDescription, 'state'))
+            disp('Using Scanimage 3.X tif reader...')
+            [header,imageStack] = scim_openTif(fname); % For Scanimage 3.X files, open with scim_openTif. ageStack output is MxNxCxK,where C spans the channel indices, and K the slice/frame indices.
+            if ndims(imageStack)>3 
+                if channels == 0; channels = 1:size(imageStack,3);end; % Load all channels
+                imageStack=permute(imageStack(:,:,channels,:),[1 2 4 5 3]); % permute to fit scimat specification
+            end
+            
+        elseif isfield(info(1), 'Software') && ~isempty(strfind(info(1).Software, 'SI.VERSION_MAJOR'))
+            disp('Using Scanimage 4/5/2016/2017/2018 tif reader...')
+            [header,imageStack,imgInfo] = scanimage.util.opentif(fname); % For Scanimage 2017 files, open with scanimage.util.opentif
 
-            if isfield(info(1), 'ImageDescription') && ~isempty(strfind(info(1).ImageDescription, 'state'))
-                disp('Using Scanimage 3.X tif reader...')
-                [header,imageStack] = scim_openTif(fname); % For Scanimage 3.X files, open with scim_openTif
-
-                if ndims(imageStack)>3 
-                    if nargin<2
-                        disp('Multicolor (or timelapse) image detected. Using first color only.')
-                        imageStack=squeeze(imageStack(:,:,1,:)); 
-                    else
-                        if channels == 0; channels = 1:size(imageStack,3);end; % Load all channels
-                        imageStack=permute(imageStack(:,:,channels,:),[1 2 4 5 3]); % permute to fit scimat specification
-                    end
-                end
-            elseif isfield(info(1), 'Software') && ~isempty(strfind(info(1).Software, 'SI.VERSION_MAJOR'))
-                disp('Using Scanimage 4/5/2016/2017/2018 tif reader...')
-                [header,imageStack,imgInfo] = scanimage.util.opentif(fname); % For Scanimage 2017 files, open with scanimage.util.opentif
-
-                if ndims(imageStack)>3 
-                    if nargin<2
-                        disp('Multicolor (or timelapse) image detected. Using first color only.')
-                        imageStack=squeeze(imageStack(:,:,1,:)); 
-                    else
-                        if channels == 0; channels = 1:size(imageStack,3);end; % Load all channels
-                        imageStack=permute(imageStack(:,:,channels,:),[1 2 4 5 3]); % permute to fit scimat specification
-                    end
-                end        
-
-            else
-                % For other TIFs, read with MATLAB TIFFclassreader
-                disp('Using generic tif reader...')
-                imageStack = zeros(info(1).Width,info(1).Height,numberOfImages, 'uint16');
-                for k = 1:numberOfImages
-                    currentImage = imread(fname, k, 'Info', info);
-                    % Widefield RGB images are converted into grayscale
-                    if size(currentImage,3)>1 
-                        currentImage=rgb2gray(currentImage);
-                    end
-                    % For some reason, widefield images are transposed
-        %             if info(1).Width == size(currentImage,2)
-        %                 currentImage=currentImage';
-        %             end
-
-                    imageStack(:,:,k) = currentImage;
-                end 
+            if ndims(imageStack)>3 
+                if channels == 0; channels = 1:size(imageStack,3);end; % Load all channels
+                imageStack=permute(imageStack(:,:,channels,:),[1 2 4 5 3]); % permute to fit scimat specification
             end
         else
-            % for Prarie z-series
-            disp('Detected Prarie tif series...')
-
-            if isempty( regexp(fname,'_Ch2_') )
-                error('File selected is not the typical Prarie Ch2 tif file. More coding required.')
-            end
-            [pathstr,name,ext] = fileparts(fname);
-            filelist=ls([pathstr '\ZSeries*_Ch2_*']);
-
-            imageStack=[];
-            numberOfImages = size(filelist,1);
+            % For other TIFs, read with MATLAB TIFFclassreader
+            disp('Using generic tif reader...')
+            imageStack = zeros(info(1).Width,info(1).Height,numberOfImages, 'uint16');
             for k = 1:numberOfImages
-                currentImage = imread([pathstr '\' filelist(k,:)]);
+                currentImage = imread(fname, k, 'Info', info);
+                disp('Widefield RGB images are converted into grayscale..')
+                if size(currentImage,3)>1 
+                    currentImage=rgb2gray(currentImage);
+                end
                 imageStack(:,:,k) = currentImage;
             end 
-
         end
-
+        pixel_size=[];
         
         
-    otherwise 
+    otherwise     % Zeiss (*.czi), OME-TIF (*.xml), etc
         % Use BIO-FORMATS Matlab toolbox to open other image formats 
         % Download at https://www.openmicroscopy.org/bio-formats/
         data = bfopen(fname);
@@ -112,6 +82,7 @@ switch ext
         end
         
         disp(['The metadata suggests a pixel size of ' num2str([ voxelSizeXdouble voxelSizeYdouble voxelSizeZdouble]) ' . Please update the settings if not correct.' ])
+        pixel_size=[ voxelSizeXdouble voxelSizeYdouble voxelSizeZdouble 1 ];
         
         % Organize image data into imagestack
         channelcount = omeMeta.getChannelCount(0);
@@ -124,11 +95,10 @@ switch ext
                 imagenumber=imagenumber+1;
             end
         end
-        if nargin<2
-            disp('Multicolor (or timelapse) image detected. Using first color only.')
-            imageStack=squeeze(imageStack(:,:,1,:)); 
-        else
+        
+        if ndims(imageStack)>3 
             if channels == 0; channels = 1:size(imageStack,3);end; % Load all channels
             imageStack=permute(imageStack(:,:,channels,:),[1 2 4 5 3]); % permute to fit scimat specification
         end
+
 end
